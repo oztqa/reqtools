5c424c062900f2bef65fdc7effa44223d3b5c5a4
gkoksharov <gkoksharov@ozon.travel>
2019-01-18 14:01:18 +0300

    fix assertion in tests

--- tests/suite_sessions.py
+++ tests/suite_sessions.py
@@ -2,33 +2,33 @@
 
 import pytest
 from requests import Session as RequestsSession
-from hamcrest import assert_that, equal_to, instance_of
+from hamcrest import assert_that, equal_to, instance_of, is_
 
-from reqtools import sessions
+from reqtools import RemoteApiSession
 
 
 def test_remote_api_create_instance():
-    session = sessions.RemoteApiSession('http://test.ru')
+    session = RemoteApiSession('http://test.ru')
     assert_that(session, instance_of(RequestsSession))
 
 
 def test_remote_api_base_url():
     url = 'http://test.ru'
-    session = sessions.RemoteApiSession(url)
-    assert_that(session.base_url, url)
+    session = RemoteApiSession(url)
+    assert_that(session.base_url, is_(equal_to(url)))
 
 
 def test_remote_api_prefix():
     url = 'http://test.ru'
     prefix = 'prefix'
-    session = sessions.RemoteApiSession(url, prefix=prefix)
-    assert_that(session.prefix, prefix)
+    session = RemoteApiSession(url, prefix=prefix)
+    assert_that(session.prefix, is_(equal_to(prefix)))
 
 
 def test_remote_api_url():
     url = 'http://test.ru'
-    session = sessions.RemoteApiSession(url)
-    assert_that(session.url, url)
+    session = RemoteApiSession(url)
+    assert_that(session.url, is_(equal_to(url)))
 
 
 @pytest.mark.parametrize(
@@ -40,13 +40,13 @@ def test_remote_api_url():
     ],
 )
 def test_remote_api_url_with_prefix(url, prefix, expected):
-    session = sessions.RemoteApiSession(url, prefix=prefix)
-    assert_that(session.url, expected)
+    session = RemoteApiSession(url, prefix=prefix)
+    assert_that(session.url, is_(equal_to(expected)))
 
 
 def test_remote_api_build_url():
-    session = sessions.RemoteApiSession('http://test.ru')
-    assert_that(session._build_url('/test'), equal_to('http://test.ru/test'))
+    session = RemoteApiSession('http://test.ru')
+    assert_that(session._build_url('/test'), is_(equal_to('http://test.ru/test')))
 
 
 @pytest.mark.parametrize(
@@ -61,5 +61,5 @@ def test_remote_api_build_url():
     ],
 )
 def test_remote_api_build_url_with_prefix(prefix, url_path, expected):
-    session = sessions.RemoteApiSession('http://test.ru', prefix=prefix)
-    assert_that(session._build_url(url_path), equal_to(expected))
+    session = RemoteApiSession('http://test.ru', prefix=prefix)
+    assert_that(session._build_url(url_path), is_(equal_to(expected)))
